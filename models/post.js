41975a313948993a582dfd9df95811d035009ff0
UberBooster <info@uberbooster.com>
2016-08-02 21:03:30 -0400

    set up schema for posts

--- models/post.js
+++ models/post.js
@@ -1,3 +1,30 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  updated: {
+    type: Date,
+    required: true
+  }
+});
+
+var Post = mongoose.model('Post', postSchema);
+module.exports = Post;
